6cfdf92ae47ebe78c91f05d0a286e429e6b162a1
Jucie-B0x <brokenbuilder@gmail.com>
2025-03-26 20:18:54 -0500

    refactor previous renditions of code

--- library.js
+++ library.js
@@ -1,6 +1,11 @@
-const myLibrary = [];
-document.getElementById("addToShelf").addEventListener("click", intake);
-let container = document.querySelector(".container");
+
+const myLibrary = [
+    new Book ('The Hobbit', 'J.R.R. Tolkien', '310', '1937'),
+    new Book ('Dune', 'Frank Herbert', '412', '1965'),
+    new Book('The Da Vinci Code', 'Dan Brown','689','2003'),
+];
+// document.getElementById("addToShelf").addEventListener("click", intake);
+let container = document.querySelector('.container');
 let shelf = document.querySelector('.shelf');
 
 
@@ -9,104 +14,29 @@ function Book(title, author,pages,publish) {
     this.author = author;
     this.pages = pages;
     this.publish = publish;
+    this.id = crypto.randomUUID();
+
 
 }
 
+function addBookToLibrary(title, author, page ,publish){
+    let newBook = new Book (title, author, page, publish);
+    myLibrary.push(newBook);
 
-function addBookToLibrary (title, author,pages,publish) {
-let newBook = new Book()
-myLibrary.push(newBook);
-id = crypto.randomUUID();
-console.log(myLibrary);
-return false;
 }
 
 function addBookToShelf () {
-   let book =  document.createElement('book')
-   for (i = 0; i < 4; i++){
-    let info = document.createElement('p')
-    info.innerText = myLibrary[i];
-    book.appendChild(info)
-   }
-   shelf.appendChild(book);
-}
-
-
-function intake () {
-    const overlay = document.createElement('div')
-    overlay.setAttribute('id', 'overlay')
-
-    const bookForm = document.createElement('form');
-    // bookForm.setAttribute ('action', '$');
-    // bookForm.setAttribute ('method', 'post');
-    bookForm.setAttribute('onsubmit', 'addBookToLibrary()')
-    bookForm.setAttribute ('id', 'bookForm')
-
-    const titleLable = document.createElement('lable');
-    titleLable.setAttribute('for', 'intakeTitle')
-    titleLable.innerText = 'Title';
-
-    const intakeTitle = document.createElement('input');
-    intakeTitle.setAttribute('type', 'text');
-    intakeTitle.setAttribute('name', 'title');
-    intakeTitle.setAttribute('id', 'intakeTitle')
-    intakeTitle.setAttribute('placeholder' , 'Book Title')
-
-
-    const authorLable = document.createElement('lable');
-    authorLable.setAttribute('for', 'intakeAuthor')
-    authorLable.innerText = 'Author';
-
-    const intakeAuthor = document.createElement('input');
-    intakeAuthor.setAttribute('type', 'text');
-    intakeAuthor.setAttribute('name', 'author');
-    intakeAuthor.setAttribute('id', 'intakeAuthor');
-    intakeAuthor.setAttribute('placeholder' , 'Author');
-
-    const pagesLable = document.createElement('lable');
-    pagesLable.setAttribute('for', 'intakePages')
-    pagesLable.innerText = 'Pages';
-   
-    const intakePages = document.createElement('input');
-    intakePages.setAttribute('type', 'number');
-    intakePages.setAttribute('name', 'pages');
-    intakePages.setAttribute('id', 'intakePages');
-    intakePages.setAttribute('placeholder' , 'Page count');
-
-    const publishLable = document.createElement('lable');
-    publishLable.setAttribute('for', 'intakePublish')
-    publishLable.innerText = 'Publish Date';
-
-    const intakePublish = document.createElement('input');
-    intakePublish.setAttribute('type', 'text');
-    intakePublish.setAttribute('name', 'publish');
-    intakePublish.setAttribute('id', 'intakePublish');
-    intakePublish.setAttribute('placeholder' , 'Publish Date');
-
-
-    const intakeSubmit = document.createElement('input');
-    intakeSubmit.setAttribute('type', 'submit');
-    intakeSubmit.setAttribute('id', 'intakeSubmit')
-    intakeSubmit.setAttribute('value', 'Submit');
-
-
-    
-    bookForm.appendChild(titleLable);
-    bookForm.appendChild(intakeTitle);
-
-    bookForm.appendChild(authorLable);
-    bookForm.appendChild(intakeAuthor);
-
-    bookForm.appendChild(pagesLable);
-    bookForm.appendChild(intakePages);
-    
-    bookForm.appendChild(publishLable);
-    bookForm.appendChild(intakePublish);
-    
-    bookForm.appendChild(intakeSubmit);
-
-    overlay.appendChild(bookForm);
-    container.appendChild(overlay)
-}
+    myLibrary.forEach((book) => {
+       let card = document.createElement('div');
+       card.classList.add('card')
+       card.innerText = book;
+       shelf.appendChild(card)
+       
+    }
+)}
 
 addBookToShelf();
+
+
+
+
